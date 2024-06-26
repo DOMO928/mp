c5f6d7fcf146d4a6974f9fd4265c3bbd76f79d2e
SOOKIE <sookie@bluebeaker.co>
2024-06-26 14:47:14 +0900

    fix: f

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 // import { ARCanvas, ARMarker } from '@artcom/react-three-arjs';
 import { BakeShadows, Environment, Merged, useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { Suspense, createContext, useContext, useEffect, useMemo, useRef } from 'react';
+import { Suspense, createContext, useContext, useEffect, useMemo, useRef, useState } from 'react';
 import * as THREE from 'three';
-import ARCanvas from './libs/arnft/arnft/components/arCanvas';
-import NFTMarker from './libs/arnft/arnft/components/nftMarker';
-import { requestCameraPermission } from './libs/util';
-import { suspend } from 'suspend-react';
+import { useARNft, useNftMarker } from './libs/arnft/arnft/arnftContext';
 import { Effects } from './libs/arnft/arnft/components/Effects';
-const asset = import('/data/Ultimate_Skies_4k_0045.exr?url').then((module) => module.default);
+import ARCanvas from './libs/arnft/arnft/components/arCanvas';
+import { requestCameraPermission } from './libs/util';
 
 const context = createContext(undefined);
-export function Instances({ children, ...props }: any) {
-  const { nodes, scene } = useGLTF('data/mp.glb');
+export function Instances({ children, url, ...props }: any) {
+  const { arEnabled } = useARNft();
+  const ref = useNftMarker(url);
+
+  const { nodes } = useGLTF('data/mp.glb');
   const instances = useMemo(() => {
-    scene.traverse((obj) => {
-      if ((obj as THREE.Mesh).isMesh)
-        ((obj as THREE.Mesh).material as THREE.MeshStandardMaterial).envMapIntensity = 1.8;
-    });
     return {
       Blackmetal: nodes.black_metal,
       Block: nodes.block,
@@ -38,15 +35,20 @@ export function Instances({ children, ...props }: any) {
     };
   }, [nodes]);
   return (
-    <Merged meshes={instances} {...props}>
-      {(instances: any) => <context.Provider value={instances} children={children} />}
-    </Merged>
+    <group ref={ref} visible={!arEnabled}>
+      <group name="center">
+        <Merged meshes={instances} {...props}>
+          {(instances: any) => <context.Provider value={instances} children={children} />}
+        </Merged>
+      </group>
+    </group>
   );
 }
 
 function Box() {
   const modelRef = useRef<THREE.Group>(null);
   const instances: any = useContext(context);
+  const [ang, setAng] = useState(0);
 
   useFrame(({ gl, camera }) => {
     if (gl) {
@@ -55,34 +57,48 @@ function Box() {
       gl.setSize(window.innerWidth, window.innerHeight);
     }
   });
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch('data/angle.json', {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        });
+        if (res) {
+          const js = await res.json();
+          setAng(js.angle);
+        }
+      } catch (e) {
+        console.log('ERROR', e);
+      }
+    };
+
+    getData();
+  }, []);
   return (
     <>
-      <NFTMarker url={'../data/marker/marker'}>
-        {/* <mesh scale={[150, 150, 150]}>
-          <primitive object={scene} position={[0, 1, 1]} rotation-x={Math.PI / 5} />
-        </mesh> */}
-
-        <group ref={modelRef} dispose={null} scale={[1.5, 1.5, 1.5]} position={[0, 1, 1]} rotation-x={Math.PI / 5}>
-          <instances.Blackmetal rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Block rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Brickwall rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Concretea rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Concreteb rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Concretec rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Cooperroof rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Dirt rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Glass rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Painteddoor rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Roof rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Wall rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Windowframe rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Windowframea rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Wood rotation={[Math.PI / 2, 0, 0]} />
-          <instances.Woodenwall rotation={[Math.PI / 2, 0, 0]} />
-        </group>
-
-        <BakeShadows />
-      </NFTMarker>
+      <group ref={modelRef} dispose={null} scale={[1.5, 1.5, 1.5]} position={[0, 1, 1]} rotation-x={ang || Math.PI / 5}>
+        <instances.Blackmetal rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Block rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Brickwall rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Concretea rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Concreteb rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Concretec rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Cooperroof rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Dirt rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Glass rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Painteddoor rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Roof rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Wall rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Windowframe rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Windowframea rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Wood rotation={[Math.PI / 2, 0, 0]} />
+        <instances.Woodenwall rotation={[Math.PI / 2, 0, 0]} />
+      </group>
+      <BakeShadows />
     </>
   );
 }
@@ -99,11 +115,10 @@ export default function App() {
       // }}
     >
       <Suspense fallback={null}>
-        <Instances>
+        <Instances url={'../data/marker/marker'}>
           <Box />
         </Instances>
-        <ambientLight intensity={0.6}/>
-        <Environment files={suspend(asset) as string} />
+        <Environment preset="park" />
         <Effects />
       </Suspense>
     </ARCanvas>
